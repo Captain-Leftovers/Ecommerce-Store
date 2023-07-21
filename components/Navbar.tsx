84c86dc92857869f463ab43e76e83efebddd4460
Dob <lalilulelo.mg@gmail.com>
2023-07-22 09:51:03 +1000

    added getCategories actions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
 					<Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
 						<p className="font-bold text-xl">STORE</p>
 					</Link>
-          <MainNav data={[]} />
+          <MainNav data={[]	} />
 				</div>
 			</Container>
 		</div>
