6928a14416aa1714fa8f1b81cc3f7a0165f63173
Dob <lalilulelo.mg@gmail.com>
2023-09-13 13:51:44 +1000

    added getBillboard action

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,19 @@
 import Container from "@/components/ui/Container";
 
 import Billboard from "@/components/Billboard";
+import getBillboard from "@/actions/getBillboard";
 
+export const revalidate = 0;
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const billboard = await getBillboard("1de051ad-d06e-493c-9eb4-39a14e2c0f35")
 
   return (
 
       <Container>
         <div className="space-y-10 pb-10 ">
-          <Billboard data={} />
+          <Billboard data={billboard} />
         </div>
       </Container>
     )
