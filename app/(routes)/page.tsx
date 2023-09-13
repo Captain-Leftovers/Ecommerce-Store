2410274f50ac5ac497fcec7d4845884f8c695cc2
Dob <lalilulelo.mg@gmail.com>
2023-09-13 13:42:53 +1000

    added billboard component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,16 @@
+import Container from "@/components/ui/Container";
 
+import Billboard from "@/components/Billboard";
 
 
 export default function HomePage() {
 
   return (
 
-        <div>Home Page for the Store</div>
-  )
+      <Container>
+        <div className="space-y-10 pb-10 ">
+          <Billboard data={} />
+        </div>
+      </Container>
+    )
 }
--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -1,17 +1,28 @@
-"use client"
+'use client'
 
+import Button from '@/components/ui/Button'
+import { ShoppingBag } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
-
-
-type NavbarActionsProps = {
-    
-  }
+type NavbarActionsProps = {}
 export default function NavbarActions({}: NavbarActionsProps) {
+	const [isMounted, setIsMounted] = useState(false)
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
 
+	if (!isMounted) {
+		return null
+	}
 
-return (
-  <div className="ml-auto flex items-center gap-x-4" >
-     NavbarActions
-  </div>
-  )
+	return (
+		<div className="ml-auto flex items-center gap-x-4 ">
+			<Button className="flex items-center rounded-full bg-black px-4 py-2">
+				<ShoppingBag size={20} color="white" />
+				<span className="ml-2 text-sm font-medium text-white">
+					0{/* TODO render amount items in cart */}
+				</span>
+			</Button>
+		</div>
+	)
 }
