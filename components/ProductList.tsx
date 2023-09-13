4f57a2e7c094f3aeb8e842b9c7419a838b8a9b8e
Dob <lalilulelo.mg@gmail.com>
2023-09-13 17:26:01 +1000

    added productCard

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,5 +1,6 @@
 import { Product } from '@/types'
 import NoResults from './ui/NoResults'
+import ProductCard from './ui/ProductCard'
 
 type ProductListProps = {
 	title: string
@@ -8,8 +9,13 @@ type ProductListProps = {
 export default function ProductList({ items, title }: ProductListProps) {
 	return (
 		<div className="space-y-4">
-			<h3 className='font-bold text-3xl' >{title}</h3>
-            {items.length === 0 && <NoResults />}
+			<h3 className="font-bold text-3xl">{title}</h3>
+			{items.length === 0 && <NoResults />}
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-4">
+				{items.map((item) => (
+					<ProductCard key={item.id} data={item} />
+				))}
+			</div>
 		</div>
 	)
 }
---
+++ components/ui/IconButton.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils'
+import { MouseEventHandler } from 'react'
+
+type IconButtonProps = {
+	onClick?: MouseEventHandler<HTMLButtonElement> | undefined
+	icon: React.ReactElement
+	className?: string
+}
+export default function IconButton({
+	icon,
+	className,
+	onClick,
+}: IconButtonProps) {
+	return (
+		<button
+			onClick={onClick}
+			className={cn(
+				'rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition',
+				className
+			)}
+		>
+			{icon}
+		</button>
+	)
+}
