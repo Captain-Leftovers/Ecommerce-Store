657264d3dd224d7ba8cdda3d6e385d503e086d7a
Dob <lalilulelo.mg@gmail.com>
2023-09-14 06:45:54 +1000

    added getProducts

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -5,6 +5,7 @@ import Image from "next/image"
 import IconButton from "./IconButton"
 import { Expand, ShoppingCart } from "lucide-react"
 import Currency from "./Currency"
+import { useRouter } from "next/navigation"
 
 
 type ProductCardProps = {
@@ -12,9 +13,13 @@ type ProductCardProps = {
   }
 export default function ProductCard({data}: ProductCardProps) {
 
+  const router = useRouter()
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`)
+  }
 
 return (
-  <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4" >
+  <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4" >
      {/* TODO Images and Actions */}
      <div className="aspect-square rounded-xl bg-gray-100 relative ">
         <Image alt="Image" src={data?.images?.[0]?.url} fill className="aspect-square object-cover rounded-md" />
