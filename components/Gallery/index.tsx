bb6f5bf2cf04496ca65d92c2d1d771b581476960
Dob <lalilulelo.mg@gmail.com>
2023-09-15 11:11:10 +1000

    added gallery tab

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { Tab } from "@headlessui/react"
 
 import { Image as ImageType } from "@/types"
+import GalleryTab from "./GalleryTab"
 
 
 type GalleryProps = {
@@ -17,7 +18,7 @@ return (
         <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none"></div>
         <Tab.List className="grid grid-cols-4 gap-6">
             {images.map((image) => (
-                <GalleryTab key={image.id} />
+                <GalleryTab key={image.id} image={image} />
             ))}
         </Tab.List>
     </Tab.Group>
