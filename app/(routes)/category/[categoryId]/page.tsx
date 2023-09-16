2890e6946bea62aa89a9055b3121f4aad3fd6ab0
Dob <lalilulelo.mg@gmail.com>
2023-09-16 18:04:08 +1000

    added mobile filters component

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,11 +1,13 @@
-import getCategory from "@/actions/getCategory"
-import getColors from "@/actions/getColors"
-import getProducts from "@/actions/getProducts"
-import getSizes from "@/actions/getSizes"
-import Billboard from "@/components/Billboard"
-import Container from "@/components/ui/Container"
-import Filter from "./components/Filter"
-
+import getCategory from '@/actions/getCategory'
+import getColors from '@/actions/getColors'
+import getProducts from '@/actions/getProducts'
+import getSizes from '@/actions/getSizes'
+import Billboard from '@/components/Billboard'
+import Container from '@/components/ui/Container'
+import Filter from './components/Filter'
+import NoResults from '@/components/ui/NoResults'
+import ProductCard from '@/components/ui/ProductCard'
+import MobileFilters from './components/MobileFilters'
 
 export const revalidate = 0
 
@@ -18,32 +20,53 @@ type CategoryPageProps = {
 		sizeId: string
 	}
 }
-export default async function CategoryPage({params, searchParams}: CategoryPageProps) {
+export default async function CategoryPage({
+	params,
+	searchParams,
+}: CategoryPageProps) {
+	const products = await getProducts({
+		categoyId: params?.categoryId,
+		colorId: searchParams?.colorId,
+		sizeId: searchParams?.sizeId,
+	})
 
-    const products = await getProducts({
-        categoyId: params?.categoryId,
-        colorId: searchParams?.colorId,
-        sizeId: searchParams?.sizeId,
-    })
-
-    const sizes = await getSizes()
-    const colors = await getColors()
-    const category = await getCategory(params?.categoryId) 
+	const sizes = await getSizes()
+	const colors = await getColors()
+	const category = await getCategory(params?.categoryId)
 
 	return (
-        <div className="bg-white ">
-            <Container>
-                <Billboard data={category.billboard} />
-                <div className="px-4 sm:px-6 lg:px-8 pb-24">
-                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-                        //TODO : add mobile filters
-                        <div className="hidden lg:block">
-                            <Filter valueKey="sizeId" name="sizes" data={sizes} />
-                            <Filter valueKey="colorId" name="Colors" data={colors} />
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        </div>
-    )
+		<div className="bg-white ">
+			<Container>
+				<Billboard data={category.billboard} />
+				<div className="px-4 sm:px-6 lg:px-8 pb-24">
+					<div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+						<MobileFilters sizes={sizes} colors={colors} />
+						<div className="hidden lg:block">
+							<Filter
+								valueKey="sizeId"
+								name="sizes"
+								data={sizes}
+							/>
+							<Filter
+								valueKey="colorId"
+								name="Colors"
+								data={colors}
+							/>
+						</div>
+						<div className="mt-6 lg:col-span-4 lg:mt-0">
+							{products?.length === 0 && <NoResults />}
+							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+								{products?.map((product) => (
+									<ProductCard
+										key={product.id}
+										data={product}
+									/>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</Container>
+		</div>
+	)
 }
