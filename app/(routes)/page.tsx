86a4d7413c2a120026d97c7fc56fa512d4cc4182
beeondweb <beeondweb@gmail.com>
2024-03-31 16:03:12 +1100

    added home page billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,14 +8,14 @@ import ProductList from '@/components/ProductList'
 export const revalidate = 0
 
 export default async function HomePage() {
-	const billboard = await getBillboard('1de051ad-d06e-493c-9eb4-39a14e2c0f35')
+	const billboard = await getBillboard('357d81c8-7510-4af6-8718-f4e6c1eff770')
 	const products = await getProducts({ isFeatured: true })
 
 	return (
 		<Container>
-			<div className="space-y-10 pb-10 ">
+			<div className="pb-10 space-y-10 ">
 				<Billboard data={billboard} />
-			<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 ">
+			<div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8 ">
 				<ProductList title="Featured Products" items={products} />
 			</div>
 			</div>
